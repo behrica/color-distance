ad7665846cb9f62be34f5d45a905e6f7562e943e
Carsten Behring <carsten.behring@gmail.com>
2017-10-26 11:12:08 +0200

    pumbed version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject behrica/color-distance "0.0.2"
+(defproject behrica/color-distance "0.0.3-SNAPSHOT"
   :description "Library to clalculate color distances"
   :min-lein-version  "2.0.0"
   :source-paths      ["src/clojure"]
