a6a2ec9837e815018e0a94e2e307fd9753f031f1
Carsten Behring <carsten.behring@gmail.com>
2017-10-26 10:58:28 +0200

    added junit testing to profile

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Library to clalculate color distances"
   :min-lein-version  "2.0.0"
   :source-paths      ["src/clojure"]
-  :java-source-paths ["src/java"]
+  :java-source-paths ["src/java" "test/java"]
   :repositories [["maven-restlet" "http://maven.restlet.org"]]
   :dependencies [
                  [junit/junit "4.11"]
@@ -16,6 +16,8 @@
                  
                  [org.apache.commons/commons-lang3 "3.4"]
                  ]
+  :plugins [[lein-junit "1.1.8"]]
+  :junit ["test/java"]
   )
 
 
