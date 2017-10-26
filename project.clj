2c4e4f23fd6384481b18aee6b653f7c70b140534
Carsten Behring <carsten.behring@gmail.com>
2017-10-26 08:56:23 +0200

    imported java code from https://github.com/rbingram/ColorDistance/

---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject behrica/color-distance "0.0.1-SNAPSHOT"
+  :description "Library to clalculate color distances"
+  :min-lein-version  "2.0.0"
+  :source-paths      ["src/clojure"]
+  :java-source-paths ["src/java"]
+  :repositories [["maven-restlet" "http://maven.restlet.org"]]
+  :dependencies [
+                 [junit/junit "4.11"]
+                 [org.hibernate/hibernate-core "5.0.0.CR2"]
+                 [org.hsqldb/hsqldb "2.3.1"]
+                 [org.restlet.jse/org.restlet "2.3.0"]
+                 [org.restlet.jse/org.restlet.ext.simple "2.3.0"]
+                 [org.restlet.jee/org.restlet.ext.servlet "2.3.0"]
+                 [org.apache.geronimo.specs/geronimo-servlet_2.5_spec "1.2"]
+                 [com.google.code.gson/gson "1.7.1"]
+                 
+                 [org.apache.commons/commons-lang3 "3.4"]
+                 ]
+  )
+
+
+
+
